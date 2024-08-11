ab67fb0e00cca452c300e2e453f14b24e7a1656c
User876543210 <sksakil545433@gmail.com>
2024-08-11 21:54:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ DB_NAME = environ.get("DB_NAME", "vjbotz")
 
 
 # Bot Information
-BOT_TOKEN = environ.get("BOT_TOKEN", "7049079885:AAEupj8RJaY34gTNrZ0_sneIdkPWvroAk9I")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 BOT_USERNAME = environ.get("BOT_USERNAME", "Asflimbot") # your bot username without @
 PICS = (environ.get('PICS', 'https://te.legra.ph/file/66604e5911df5a0c43ffa.jpg https://graph.org/file/f5a26ac21b060d5787ea3.jpg https://graph.org/file/30539d8f1fc92423d6a19.jpg https://graph.org/file/701ff72fb43d85e073f03.jpg')).split() # Bot Start Picture
 
@@ -81,9 +81,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "troubled-tabbatha-akasuflim2-9db095f1.koyeb.app/"
+        URL = ""
     else:
-        URL = "troubled-tabbatha-akasuflim2-9db095f1.koyeb.app/"
+        URL = ""
 
 
 
